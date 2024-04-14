17c32b0b6664df03942825a479a3ebcd69908e2b
Fryderyk Jellinek <fryderyk.jellinek@gmail.com>
2024-04-14 21:21:37 +0200

    added basic look for MobileNav

--- src/components/MobileNav.jsx
+++ src/components/MobileNav.jsx
@@ -1,15 +1,15 @@
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
 import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
+
 import { useSelector, useDispatch } from 'react-redux';
 import { setClose } from '../redux/reducers/interfaceSlice';
+
+import { routes } from '../routes';
+
+import { Link as ReactRouterLink } from 'react-router-dom';
 
 const MobileNav = () => {
   const isOpen = useSelector((state) => state.interface.isOpen);
@@ -22,26 +22,10 @@ const MobileNav = () => {
   const DrawerList = (
     <Box sx={{ width: 250, mt: 5}} role="presentation" component='nav'>
       <List>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List>
-        {['All mail', 'Trash', 'Spam'].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
+        {routes.map((route) => (
+          <ListItem key={route.name} disablePadding sx={{ fontWeight: 600, color: 'text.secondary', textTransform: 'uppercase'}}>
+            <ListItemButton component={ReactRouterLink} to={route.path} onClick={closeDrawer}>
+            {route.name}
             </ListItemButton>
           </ListItem>
         ))}
---
+++ src/routes.js
@@ -0,0 +1,13 @@
+import HomePage from "./pages/HomePage";
+import ContactPage from "./pages/ContactPage";
+
+export const routes = [
+    {   name: 'Home',
+        path: '/',
+        element: HomePage
+    },
+    {   name: 'Contact',
+        path: "contact",
+        element: ContactPage,
+      },
+]
