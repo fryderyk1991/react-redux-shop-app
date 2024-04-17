04b01499c16dae4bb19a2cc3360bf350dd671f60
Fryderyk Jellinek <fryderyk.jellinek@gmail.com>
2024-04-17 12:46:54 +0200

    added slice method to gridItems

--- src/components/GridContainer.jsx
+++ src/components/GridContainer.jsx
@@ -4,11 +4,17 @@ import { useSelector } from "react-redux";
 import GridCard from "./GridCard";
 
 const GridContainer = () => {    
+  
   const gridItems = useSelector(state => state.products.productsData); 
+  const startIndex = 2;
+  const endIndex = 51;
+
+  const slicedArray = gridItems.slice(startIndex, endIndex)
+
 
   return (
     <Grid container my={5} direction='column' alignItems="center" justifyContent="space-around">
-        {gridItems.map((item) => (
+        {slicedArray.map((item) => (
             <GridCard key={item.id} src={item.images[0]} title={item.title} />
         ))}
 
