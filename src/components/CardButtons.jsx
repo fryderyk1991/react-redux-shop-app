839885ef82ae689ee4667d67107970cc1a43d6a3
Fryderyk Jellinek <fryderyk.jellinek@gmail.com>
2024-04-24 07:51:49 +0200

    added framer motion package

--- src/components/CardButtons.jsx
+++ src/components/CardButtons.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types'
-import { IconButton } from "@mui/material";
+import { IconButton, Box, Button, Typography } from "@mui/material";
 
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import ShoppingBagOutlinedIcon from "@mui/icons-material/ShoppingBagOutlined";
@@ -14,14 +14,15 @@ const CardButtons = ( { properIcons } ) => {
         </IconButton>
       )}
       {properIcons === 'both' && (
-        <>
-          <IconButton sx={{ color: 'primary.main'}}>
-            <FavoriteBorderIcon />
-          </IconButton>
-          <IconButton sx={{ color: 'primary.main' }}>
-            <ShoppingBagOutlinedIcon />
-          </IconButton>
-        </>
+        <Box sx={{display: 'flex', justifyContent: 'space-between', mt: 3}}>
+            <Button variant='outlined' sx={{ flex: 3, p: 1.5}}>
+            <ShoppingBagOutlinedIcon sx={{ mr:  2 }} />
+            <Typography >Add to Cart</Typography> 
+            </Button>
+          <Button variant='outlined' sx={{ flex: 0, p: 1.5, ml: 1 }}>
+          <FavoriteBorderIcon />
+          </Button>
+        </Box>
       )}
       </> 
 
