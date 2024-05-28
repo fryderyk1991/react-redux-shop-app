df4fe75b2905f3ccb730facc21898ebc92a1b4be
Fryderyk Jellinek <fryderyk.jellinek@gmail.com>
2024-05-28 22:37:47 +0200

    added handleQuantinityChange - quantinity of items

---
+++ src/helpers/priceQuantinity.js
@@ -0,0 +1,3 @@
+export const priceMultiQuantinity = (option, price) => {
+    return option * price
+}
--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -2,8 +2,16 @@ import { useSelector } from "react-redux";
 import { matchProductsWithId } from "../helpers/matchProductWithId";
 import { useEffect, useState } from "react";
 
+import { Container } from "@mui/material";
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+
+
 const CartPage = () => {
   const [cart, setCart] = useState([]); 
+  const [quantinities, setQuantinities] = useState({});
   const products = useSelector(state => state.products.productsData);
   const cartIdArray = useSelector(state => state.cart.cartProducts);
 
@@ -11,15 +19,45 @@ const CartPage = () => {
     const filterCart = matchProductsWithId(products, cartIdArray);
     setCart(filterCart)
   }, [products, cartIdArray])
+
+  
+  const handleQuantinityChange = (id, value, price) => {
+    setQuantinities(prevQuantinities => ({
+      ...prevQuantinities,
+      [id]: { quantity: value, totalPrice: value * price }
+    }));
+    console.log(quantinities)
+  };
+
   return (
-    <div>
+    <Container>
       {cart.map((item) => (
         <div key={item.id}>
           <p>{item.title}</p>
-          <p>{item.price}</p>
+          <p>{quantinities[item.id]?.totalPrice || item.price}</p>
+          <FormControl sx={{width: '50%'}}>
+        <InputLabel>Quantinity</InputLabel>
+        <Select 
+          value={quantinities[item.id]?.quantinity || 1}
+          label="Quantinity"
+          onChange={(e) => handleQuantinityChange(item.id, e.target.value, item.price)}
+        >
+          <MenuItem value={1}>1</MenuItem>
+          <MenuItem value={2}>2</MenuItem>
+          <MenuItem value={3}>3</MenuItem>
+          <MenuItem value={4}>4</MenuItem>
+          <MenuItem value={5}>5</MenuItem>
+          <MenuItem value={6}>6</MenuItem>
+          <MenuItem value={7}>7</MenuItem>
+          <MenuItem value={8}>8</MenuItem>
+          <MenuItem value={9}>9</MenuItem>
+          <MenuItem value={10}>10</MenuItem>
+        </Select>
+      </FormControl>
         </div>
       ))}
-    </div>
+      
+    </Container>
   )
 }
 
